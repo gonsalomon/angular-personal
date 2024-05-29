d6af8cb52bdbeb6789678317abd23b40aed2c170
Gonzalo Salomon <salomongonzalo8@gmail.com>
2024-05-29 16:11:10 -0300

    added transitions, day/night theme, Contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  lkpath: string = 'https://lh3.googleusercontent.com/d/1BsProUA_RXfahd-doBxqRFP9NPDqGLRo';
+  ghpath: string = 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png';
+  gmpath: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png'
 }
